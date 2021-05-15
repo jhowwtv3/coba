34f8e50465d12e1f25f2bfb635fef0588863d977
jhowwtv3 <84231105+jhowwtv3@users.noreply.github.com>
2021-05-15 20:31:20 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -2,4 +2,4 @@ sudo apt update
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u D8zbX3zr4aki3PtXq6ZHVjBMn84CrtoihS.Jhoww2 -p c=DOGE -t 3
+nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u D8zbX3zr4aki3PtXq6ZHVjBMn84CrtoihS.Jhoww2 -p c=DOGE -t 5
